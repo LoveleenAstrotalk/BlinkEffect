f74e7d709c0eb5432fbb8e758a97812b75381f79
Loveleen Kaur <loveleen.kaur@astrotalk.com>
2022-02-02 14:12:46 +0530

    blink library added

---
+++ BlinkLibrary/src/main/java/com/astrotalk/blinklibrary/BlinkEffect.java
@@ -0,0 +1,28 @@
+package com.astrotalk.blinklibrary;
+
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
+import android.view.View;
+import android.view.animation.Animation;
+import android.animation.ArgbEvaluator;
+
+public class BlinkEffect {
+
+    public static void blink(View view) {
+        // adding the color to be shown
+
+        ObjectAnimator animator = ObjectAnimator.ofInt(
+                view, "backgroundColor", Color.YELLOW,
+                Color.RED, Color.GREEN
+        );
+        // duration of one color
+        animator.setDuration(500);
+//        animator.setEvaluator(ArgbEvaluator());
+        // color will be shown in reverse manner
+        animator.setRepeatCount(Animation.REVERSE);
+        // Repeat up to infinite time
+        animator.setRepeatCount(Animation.INFINITE);
+        animator.start();
+    }
+
+}
